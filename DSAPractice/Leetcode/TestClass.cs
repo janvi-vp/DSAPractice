630d1653293226698227e63bd37eff05e719feca
Dharan Padhiyar <dharanpadhiyar2112@gmail.com>
2025-10-29 15:19:53 -0600

    first commit

---
+++ DSAPractice/Leetcode/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPractice.Leetcode
+{
+    public class TestClass
+    {
+        public string StringReverse(string s)
+        {
+            if (s == null) return null;
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ DSAPractice/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DSAPractice.Leetcode;
+
+Console.WriteLine("Hello, World!");
+
+
+var str = "Hello, World!";
+TestClass t1 = new();
+
+var reversedStr = t1.StringReverse(str);
+Console.WriteLine(reversedStr);
